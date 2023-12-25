c42d33bf1f1b5f98362817861b33108fd4cd9b5b
mail1051741 <mail@julius-braun.com>
2023-12-25 03:03:59 +0100

    first layout of webapp

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
+# DB
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, what a great World!</p>"
+    return flask.render_template("index.html")
 
 @app.route("/<name>")
 def user(name):   
-    return "Hello "+ name + "!"
+    return flask.render_template("user.html", name=name, average = 120)
+
+@app.route("/<name>/history")
+def history(name):   
+    return name + " your last throws are listed here"
+
+@app.route("/<name>/statistics")
+def stats(name):   
+    return name + " your stats are listed here"
 
 
 if __name__ == "__main__":
