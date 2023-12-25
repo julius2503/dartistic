a274364c9bf12f37423dafc52d5021030ea2498a
mail1051741 <mail@julius-braun.com>
2023-12-25 21:10:33 +0100

    created db and designed input board

--- app/main.py
+++ app/main.py
@@ -1,24 +1,40 @@
-import flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 # DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+app.app_context().push()
+
+class Throw(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user = db.Column(db.String(200))
+    dartOne = db.Column(db.String(200))
+    dartTwo = db.Column(db.String(200))
+    dartThree = db.Column(db.String(200))
+    value = db.Column(db.Integer)
+    time = db.Column(db.DateTime(), default=datetime.utcnow)
+
 
 @app.route("/")
 def hello_world():
-    return flask.render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/<name>")
 def user(name):   
-    return flask.render_template("user.html", name=name, average = 120)
+    return render_template("user.html", name=name)
 
 @app.route("/<name>/history")
 def history(name):   
-    return name + " your last throws are listed here"
+    return render_template("history.html", name=name)
 
 @app.route("/<name>/statistics")
 def stats(name):   
-    return name + " your stats are listed here"
+    return render_template("statistics.html", name=name)
 
 
 if __name__ == "__main__":
